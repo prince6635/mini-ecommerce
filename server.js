10daf4286025a202a88a84d6f2d3ac4c35b2a70a
Zi Wang <ziwang@uber.com>
2016-09-05 16:43:29 -0700

    Add home route

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ var express = require('express');
 
 var app = express();
 
+app.get('/', function (req, res) {
+    var name = "Z";
+    res.json("My name is " + name);
+})
+
 app.listen(3000, function (err) {
     if (err) throw err;
     console.log("Server is running on port 3000");
